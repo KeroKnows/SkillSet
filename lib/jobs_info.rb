14e3f9220e5c0d516342685e370ee706b3a816e2
Simon Lee <simon2020291@gmail.com>
2021-10-16 16:01:49 +0800

    style: make rubocop happy

--- lib/jobs_info.rb
+++ lib/jobs_info.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 require 'http'
 require 'yaml'
 
 def main
   query = File.read('config/query.txt')
-  response = HTTP.post('https://api.graphql.jobs/', :json => { :query => query })
+  response = HTTP.post('https://api.graphql.jobs/', json: { query: query })
   results = response.parse
   data = results_to_data(results)
   File.write('spec/fixtures/graphql_jobs_results.yml', data.to_yaml)
@@ -15,12 +17,12 @@ end
 
 def parse_job(job)
   {
-    "title" => job['title'],
-    "tags" => job['tags'].map { |tag| tag['name'] },
-    "description" => job['description'],
-    "company" => job['company']['name'],
-    "location" => job['locationNames'],
-    "posted_date" => job['postedAt'].split('T').first
+    'title' => job['title'],
+    'tags' => job['tags'].map { |tag| tag['name'] },
+    'description' => job['description'],
+    'company' => job['company']['name'],
+    'location' => job['locationNames'],
+    'posted_date' => job['postedAt'].split('T').first
   }
 end
 
