d70874ad8e449293371e43c6b6b5c60eb62860c7
Simon Lee <simon2020291@gmail.com>
2021-10-17 10:04:36 +0800

    feat: add path checker to ensure path to `fixtures` exist before request data

--- lib/jobs_info.rb
+++ lib/jobs_info.rb
@@ -3,12 +3,19 @@
 require 'http'
 require 'yaml'
 
+FIXTURE_PATH = 'spec/fixtures'
+
 def main
+  fixture_dir_exist?
   query = File.read('config/jobs_info_query.txt')
   response = HTTP.post('https://api.graphql.jobs/', json: { query: query })
   results = response.parse
   data = results_to_data(results)
   File.write('spec/fixtures/graphql_jobs_results.yml', data.to_yaml)
+end
+
+def fixture_dir_exist?
+  raise "directory of fixtures: #{FIXTURE_PATH} does NOT exist. please CREATE IT FIRST." unless Dir.exist? FIXTURE_PATH
 end
 
 def results_to_data(results)
